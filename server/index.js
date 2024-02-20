f291878ca626f4cd0794479427c1a3f6a10cb748
ZalPadhiyar007 <zalakpadhiar111@gmail.com>
2024-02-20 01:44:29 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/Login')
+
+const UserSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const UserModel = mongoose.model("users", UserSchema)
+
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    UserModel.findOne({email: email})
+    .then(user => {
+        if(user) {
+            if(user.password === password) {
+                res.json("Login Successfully")
+            } else {
+                res.json("The password is incorrect")
+            }
+        } else {
+            res.json("No record found")
+        }
+    })
+})
+
+app.listen(3001, () => {
+    console.log("Server is running!");
+})
